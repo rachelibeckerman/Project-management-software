222b0fc774f699ac14d7213b749f07ddd790117f
rachelibeckerman <Hand4c3@mbjcomp.org.il>
2023-10-31 20:03:48 +0200

    EXE0

---
+++ Stage0/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome0554();
+            Welcome8381();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome8381();
+        private static void Welcome0554()
+        {
+            Console.Write("Enter your name: ");
+            string myString = Console.ReadLine();
+            Console.WriteLine($"{myString}, welcome to my first console application");
+        }
+
+    }
+}
