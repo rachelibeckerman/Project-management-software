7627e344286f2ebda2b0a8ad0c2dd24bcf014740
rachelibeckerman <Hand4c3@mbjcomp.org.il>
2023-11-01 16:22:23 +0200

    פרק 6

---
+++ DalList/DataSource.cs
@@ -0,0 +1,21 @@
+﻿namespace Dal;
+using DO;
+
+internal static class DataSource
+{
+    internal static List<Engineer>? Engineers = new ();
+    internal static List<Task>? Tasks = new();
+    internal static List<Dependency>? Dependencies = new();
+
+    internal static class Config
+    {
+        internal const int startTaskId = 1;
+        private static int nextTaskId = startTaskId;
+        internal static int NextTaskId { get => nextTaskId++;  }
+
+        internal const int startDependencyId = 1;
+        private static int nextDependencyId = startDependencyId;
+        internal static int NextDependencyId { get => nextDependencyId++; }
+    }
+
+}
